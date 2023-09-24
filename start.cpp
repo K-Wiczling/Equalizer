b920ecd6217690259a244dfbdc3c821950691e0d
k-wiczling <wiczling.karol@gmail.com>
2023-09-24 14:25:42 +0200

    simple menu

--- start.cpp
+++ start.cpp
@@ -1,6 +1,33 @@
 #include  <iostream>
+#include <string>
 using namespace std;
+
+string menu[3] = {"Play", "Crypt", "Exit"};
+int menuImput = 0;
+string audioLocatin = "";
 int main () {
-  cout << "Equalizer!" << endl ; 
+
+  cout << "Welcome to Crypto Equalizer"<< endl ;
+  cout << "Give me location of the audio file" << endl ;
+  cin >> audioLocatin;
+  int i = 1;
+  for (string item : menu) {
+    cout << i << ". " + item << endl;
+    ++i;
+  }
+ 
+  cin >> menuImput;
+  switch (menuImput)
+  {
+  case 1:
+    cout << "Playing the song" << endl;
+    break;
+  case 2:
+    cout << "Give me message to encrypt" << endl;
+    break;
+  
+  default:
+    break;
+  }
   return 0;
 }
